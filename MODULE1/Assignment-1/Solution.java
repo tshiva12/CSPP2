0de3e1492897d464ebe9f1b11852a38188bbdef5
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-27 23:05:27 +0530

    rollname

---
+++ MODULE1/Assignment-1/Solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		int number = s.nextInt();
+		System.out.println("Name : " + name + "Roll Number : "+ number);
+	}
+}
