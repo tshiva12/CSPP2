9c6b00bd23c51675c09df4d5881123f5273e30d8
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-27 23:00:06 +0530

    swap

---
+++ MODULE1/swap.java
@@ -0,0 +1,13 @@
+class swap{
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+}
