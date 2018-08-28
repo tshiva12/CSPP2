fa72faa439bef975fa5e856a3d99b631fd77995a
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-28 15:45:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- MODULE2/Assignment-2/Solution.java
+++ MODULE2/Assignment-2/Solution.java
@@ -3,35 +3,35 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/*
-	Do not modify this main function.
-	@param      args  The arguments
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	public static void rootsOfQuadraticEquation(final int a, final int b, final int c)
-	{
-		double root1;
-		double root2;
-		int determinant = b * b - (4 * a * c);
-		root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+    /*
+    Do not modify this main function.
+    @param      args  The arguments
+    */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+     * { function_description }
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    public static void rootsOfQuadraticEquation(final int a, final int b, final int c)
+    {
+        double root1;
+        double root2;
+        int determinant = b * b - (4 * a * c);
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a);
         root2 = (-b - Math.sqrt(determinant)) / (2 * a);
         System.out.format(root1 + " " + root2);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation
-	function and print the output.
-	*/
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation
+    function and print the output.
+    */
 }
