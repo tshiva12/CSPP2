49983025c0693d975c207707b409d02c53b10b4c
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-28 15:35:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- MODULE2/Assignment-3/Solution.java
+++ MODULE2/Assignment-3/Solution.java
@@ -12,10 +12,10 @@ public class Solution {
 	 * @param      args  The arguments
 	 */
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in); 
+		Scanner s = new Scanner(System.in); 
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base,exponent);
         System.out.println(result);
 	}
 	/*
