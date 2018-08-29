f1445223342bac3eb8edfe21fe6fd91e69511928
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-29 12:24:54 +0530

    Even

---
+++ CSPP2_practices/MODULE3/ConverttoWhileLoop.java
@@ -0,0 +1,12 @@
+public class ConverttoWhileLoop {
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<=10){
+			if(i%2 == 0){
+				System.out.println(i);
+			}
+			i = i+1;
+		}
+		System.out.println("GoodBye!");
+	}
+}
