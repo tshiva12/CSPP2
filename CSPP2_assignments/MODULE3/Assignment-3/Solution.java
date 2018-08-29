a1a7b61fedd34ce7d79c22b2d3ce4e6d92f41f8b
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-29 11:47:25 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2_assignments/MODULE3/Assignment-3/Solution.java
+++ CSPP2_assignments/MODULE3/Assignment-3/Solution.java
@@ -1,22 +1,42 @@
-
 import java.util.*;
 /*
-	Do not modify this main function.
-	*/
-public class Solution {
-
-    public static void main(String[] args) {
+    Do not modify this main function.
+    */
+/**
+ * Main class.
+ */
+public final class Solution {
+    /**
+     * @Constructs the object.
+     */
+    private Solution() {
+        //constructor is not used
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
         Scanner s=new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int gcd = gcdoftwonum(n1,n2);
+        int gcd = gcdoftwonum(n1, n2);
         System.out.println(gcd);
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
-    public static int gcdoftwonum(int n1, int n2) {
+    Need to write the gcd function and print the output.
+    */
+    /**
+     * gcd of two numbers function.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int gcdoftwonum(final int n1, final int n2) {
         int temp = 0;
         for(int i = 1; i <= n1 && i <= n2; i++)
         {
