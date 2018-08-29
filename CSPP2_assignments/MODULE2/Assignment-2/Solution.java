c309eb90b9d47ab63e4ab3c4a49f01d0103d952d
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-29 13:50:51 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

---
+++ CSPP2_assignments/MODULE2/Assignment-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * @Constructs the object.
+     */
+    private Solution() {
+        //constructor is not used
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    public static void roots(final int a, final int b, final int c) {
+        double root1;
+        double root2;
+        int determinant = b * b - (2 * 2 * a * c);
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+        root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+        System.out.println(root1 + " " + root2);
+    }
+}
+// program ends
+
+
+
+
