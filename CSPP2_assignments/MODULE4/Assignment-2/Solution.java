5e3617e346de16893a4e4b16d72af4908e99d39d
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-30 14:06:48 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ CSPP2_assignments/MODULE4/Assignment-2/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of rows and columns of a matrix");
+		int rows = s.nextInt();
+		int cols = s.nextInt();
+		int a[][] = new int[rows][cols];
+		int b[][] = new int[rows][cols];
+		int sum[][] = new int[rows][cols];
+	}
+}
