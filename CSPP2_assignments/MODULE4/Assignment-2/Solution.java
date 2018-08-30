434c77af677797f88a15d6dc68c528f88b591e51
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-30 15:27:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- CSPP2_assignments/MODULE4/Assignment-2/Solution.java
+++ CSPP2_assignments/MODULE4/Assignment-2/Solution.java
@@ -45,8 +45,7 @@ public final class Solution {
                 }
                 System.out.println();
             }
-        }
-        else {
+        } else {
             System.out.println("not possible");
         }
     }
