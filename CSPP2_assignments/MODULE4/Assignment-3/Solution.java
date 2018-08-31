817bc59b2865958c974c050a507bb99e668c5fec
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-31 16:49:32 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- CSPP2_assignments/MODULE4/Assignment-3/Solution.java
+++ CSPP2_assignments/MODULE4/Assignment-3/Solution.java
@@ -7,47 +7,48 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//constructor is not used.
-	}
-	/**
-	 * Main function of class.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * Binary to decimal Conversion function.
-	 *
-	 * @param      s     s is a input string.
-	 *
-	 * @return     return is used to return the value
-	 */
-	public static String binaryToDecimal(String s) {
-		double sum = 0 ;
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			int num = Character.getNumericValue(ch);
-			sum = sum + (num * Math.pow(2, (s.length() - 1) - i));
-		}
-		int temp = (int) sum;
-		String res = Integer.toString(temp);
-		return res;
-	}
+public final class Solution {
+    /*
+    Do not modify this main function.
+    */
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor is not used.
+    }
+    /**
+     * Main function of class.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+            //Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * Binary to decimal Conversion function.
+     *
+     * @param      s     s is a input string.
+     *
+     * @return     return is used to return the value
+     */
+    public static String binaryToDecimal(final String s) {
+        double sum = 0 ;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int num = Character.getNumericValue(ch);
+            sum = sum + (num * Math.pow(2, (s.length() - 1) - i));
+        }
+        int temp = (int) sum;
+        String res = Integer.toString(temp);
+        return res;
+    }
 }
