66051cf5ab655d70b7877b68d527db7631d2a8cb
tshiva12 <shivaprasad233307@msitprogram.net>
2018-09-03 18:37:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2_assignments/MODULE7/Assignment-1/Solution.java
+++ CSPP2_assignments/MODULE7/Assignment-1/Solution.java
@@ -1,46 +1,56 @@
+/**
+ * @author tshiva12
+ */
 import java.util.*;
 /**
  * Class for input validator.
  */
 class InputValidator {
-	/*Write the atrributes and methods for InputValidator*/
-	/**
-	 * constructor is used.
-	 */
-	private String input;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      str1  The string 1 is a input string.
-	 */
-	InputValidator(final String str1) {
-		this.input = str1;
-	}
-	/**
-	 * boolean function of validateData that prints true or false
-	 *
-	 * @return     return the true or false based on the condition
-	 */
-	boolean validateData() {
-    	if (input.length() >= 6) {
-    		return true;
-    	} else {
-    		return false;
-    	}
-    }	
+    /*Write the atrributes and methods for InputValidator*/
+    /**
+     * constructor is used.
+     */
+    private String input;
+    /**
+     * Constructs the object.
+     *
+     * @param      str1  The string 1 is a input string.
+     */
+    InputValidator(final String str1) {
+        this.input = str1;
+    }
+    /**
+     * boolean function of validateData that prints true or false.
+     *
+     * @return     return the true or false based on the condition
+     */
+    boolean validateData() {
+        final int max = 6;
+        if (input.length() >= max) {
+            return true;
+        } else {
+            return false;
+        }
+    }   
 }
 /**
  * main class Solution.
  */
 public final class Solution {
-	/**
-	 * main function.
-	 */
-	public static void main(final String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor is used.
+    }
+    /**
+     * main function.
+     */
+    public static void main(final String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println(i.validateData());
     }
 } 
 
