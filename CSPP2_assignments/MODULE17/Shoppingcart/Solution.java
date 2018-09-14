41f80252ab8aa40c53e42f83fb7e9ddfcafe0b49
tshiva12 <shivaprasad233307@msitprogram.net>
2018-09-14 22:10:33 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2_assignments/MODULE17/Shoppingcart/Solution.java
+++ CSPP2_assignments/MODULE17/Shoppingcart/Solution.java
@@ -1,47 +1,49 @@
 import java.util.Scanner;
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ShoppingCart shopcart = new ShoppingCart();
-		while (sc.hasNext()) {
-			String input = sc.nextLine();
-			String[] tokens = input.split(" ");
-			switch(tokens[0]) {
-				case "Item":
-				String[] data = tokens[1].split(",");
-				shopcart.addToCatalog(new Item(data[0],
-				 Integer.parseInt(data[1]),
-				  Double.parseDouble(data[2])));
-				break;
-				case "catalog":
-				shopcart.showCatalog();
-				break;
-				case "add":
-				data = tokens[1].split(",");
-				shopcart.addToCart(new Item(data[0],
-				 Integer.parseInt(data[1])));
-				break;
-				case "show":
-				shopcart.showCart();
-				break;
-				case "totalAmount":
-				System.out.println("totalAmount: " + shopcart.getTotalAmount());
-				break;
-				case "remove":
-				data = tokens[1].split(",");
-				shopcart.removeFromCart(new Item(data[0],
-				 Integer.parseInt(data[1])));
-				break;
-				case "payableAmount":
-				System.out.println("Payable amount: " + shopcart.getPayableAmount());				
-				break;
-				case "print":
-				shopcart.printInvoice();
-				break;
-				case "coupon":
-				shopcart.applyCoupon(tokens[1]);
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ShoppingCart shopcart = new ShoppingCart();
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String[] tokens = input.split(" ");
+            switch(tokens[0]) {
+                case "Item":
+                String[] data = tokens[1].split(",");
+                shopcart.addToCatalog(new Item(data[0],
+                 Integer.parseInt(data[1]),
+                  Double.parseDouble(data[2])));
+                break;
+                case "catalog":
+                shopcart.showCatalog();
+                break;
+                case "add":
+                data = tokens[1].split(",");
+                shopcart.addToCart(new Item(data[0],
+                 Integer.parseInt(data[1])));
+                break;
+                case "show":
+                shopcart.showCart();
+                break;
+                case "totalAmount":
+                System.out.println("totalAmount: " +
+                 shopcart.getTotalAmount());
+                break;
+                case "remove":
+                data = tokens[1].split(",");
+                shopcart.removeFromCart(new Item(data[0],
+                 Integer.parseInt(data[1])));
+                break;
+                case "payableAmount":
+                System.out.println("Payable amount: " +
+                 shopcart.getPayableAmount());
+                break;
+                case "print":
+                shopcart.printInvoice();
+                break;
+                case "coupon":
+                shopcart.applyCoupon(tokens[1]);
+                break;
+            }
+        }
+    }
 }
