edbc62e88b538eafdf50685da6e6a97687c063f5
tshiva12 <shivaprasad233307@msitprogram.net>
2018-09-19 23:54:59 +0530

    FitByte

---
+++ CSPP2_assignments/MODULE21/FITBYTE DASHBOARD/Waterlog.java
@@ -0,0 +1,19 @@
+class Waterlog {
+	private double consumption;
+	// private String time;
+	// private String date;
+	Waterlog(double consumption) {
+		this.consumption = consumption;
+		// this.time = time;
+		// this.date = date;
+	}
+	public double getConsumption() {
+		return this.consumption;
+	}
+// 	public String getTime() {
+// 		return this.time;
+// 	}
+// 	public String getDate() {
+// 		return this.date;
+// 	}
+}
