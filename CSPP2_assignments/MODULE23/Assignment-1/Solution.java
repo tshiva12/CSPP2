8af51eecd43026f609b9ecd142218fe209f0ae2e
tshiva12 <shivaprasad233307@msitprogram.net>
2018-09-21 21:36:58 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- CSPP2_assignments/MODULE23/Assignment-1/Solution.java
+++ CSPP2_assignments/MODULE23/Assignment-1/Solution.java
@@ -15,57 +15,39 @@ public final class Solution {
         Scanner scan = new Scanner(System.in);
         File files = new File(scan.nextLine());
         File[] testfiles = files.listFiles();
-        // System.out.println(Arrays.toString(testfiles));
-        // System.out.print("\t\t");
         for (File filename : testfiles) {
             System.out.print("      \t" + filename.toString().split("\\\\")[1]);
-
         }
         for (File each : testfiles) {
             String input1 = each.getAbsolutePath();
-            // System.out.println(input1);
             System.out.println();
-
             System.out.print(each.toString().split("\\\\")[1] + "       \t");
             for (File next : testfiles) {  
                 String input2 = next.getAbsolutePath();
-                // System.out.println(input2);
                 try {
                     FileReader fr1 = new FileReader(input1);
                     FileReader fr2 = new FileReader(input2);
-
                     BufferedReader br1 = new BufferedReader(fr1);
                     BufferedReader br2 = new BufferedReader(fr2);
                     HashMap<String, Integer> hm1 = new HashMap<>();
                     HashMap<String, Integer> hm2 = new HashMap<>();
-
                     String str;
                     while ((str = br1.readLine()) != null ) {
                         String [] line = str.replaceAll("[^a-zA-Z0-9_ ]", "").toLowerCase().split(" ");
-                        // String[] line = str.toLowerCase().split(" ");
                         for (String word : line) {
                             if (hm1.containsKey(word)) {
                                 hm1.put(word, hm1.get(word) + 1);
-
                             } else {
                                 hm1.put(word, 1);
                             }
                             if (!hm2.containsKey(word)) {
                                 hm2.put(word, 0);
-
                             }
-
                         }
-                        // System.out.println(freq);
-
                     }
 
                     while ((str = br2.readLine()) != null ) {
                         String [] str1 = str.replaceAll("[^a-zA-Z0-9_ ]", "").toLowerCase().split(" ");
-
-
-                        // String [] line = str.toLowerCase().split(" ");
-
                         for (String word : str1) {
                             if (hm2.containsKey(word)) {
                                 hm2.put(word, hm2.get(word) + 1);
@@ -75,12 +57,8 @@ public final class Solution {
                             }
                             if (!hm1.containsKey(word)) {
                                 hm1.put(word, 0);
-
                             }
-
                         }
-                        // System.out.println(freq2);
-
                     }
                     System.out.print(plagarismpercentage(hm1, hm2) + "\t");
                 } catch (FileNotFoundException e) {
@@ -94,18 +72,14 @@ public final class Solution {
             }
 
         }
-        System.out.println();
-        // String input1 = "dfghjklkhgvfcghj/fghjklkjh/";
-        //String input2 = "/Users/harinathareddy/Desktop/MSIT COURSES/CSPP2_20186033/cspp2-assignments/m23/input002.txt";
-
-    }
+        System.out.println();    }
     /**
      * plagrism percentage calculation.
      *
      * @param      freq   The frequency
      * @param      freq2  The frequency 2
      *
-     * @return     { description_of_the_return_value }
+     * @return     return the plagarism percentage.
      */
     public static int plagarismpercentage(final HashMap<String, Integer> hm1,
                                    final HashMap<String, Integer> hm2) {
@@ -113,15 +87,10 @@ public final class Solution {
         int sum2 = 0;
         int dot = 0;
         final int hundred = 100;
-        // System.out.println(freq + "    " + freq2);
         for (String str2 : hm1.keySet()) {
             sum1 += hm1.get(str2) * hm1.get(str2);
-            // System.out.println(sum);
             sum2 += hm2.get(str2) * hm2.get(str2);
-            // System.out.println(sum2);
             dot += hm1.get(str2) * hm2.get(str2);
-            // System.out.println(dot);
-
         }
         return ((int)((dot) / (Math.sqrt(sum1) * Math.sqrt(sum2)) * hundred));
     }
